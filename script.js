ec69df600b0060460658e96181e1bd8f198ab19f
karmas1207 <karomassudi@live.se>
2021-04-22 14:52:09 +0200

    Fjärde commit
    Lagt till formen för ett kontaktformulär, en karta samt mycket bättre responsiv design.
    70% klar på design.
    20% klar på funktionalitet.

--- script.js
+++ script.js
@@ -20,8 +20,22 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
+    if (slides.length == slideIndex) {slideIndex = 1}
   }
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3500);
 }
+
+$(function() {
+
+  $(".form-control").on('focus', function(){
+    $(this).parents(".form-group").addClass('focused');
+  });
+
+  $(".form-control").on('blur', function(){
+    $(this).parents(".form-group").removeClass('focused');
+  });
+
+});
